6457662963ea04a3d9129b4200e750b74ba4ad30
MrPowers <matthewkevinpowers@gmail.com>
2017-05-17 00:17:19 -0500

    Overload the createDF method to allow for syntaxes that are even more concise

--- src/main/scala/com/github/mrpowers/spark/daria/sql/SparkSessionExt.scala
+++ src/main/scala/com/github/mrpowers/spark/daria/sql/SparkSessionExt.scala
@@ -2,7 +2,7 @@ package com.github.mrpowers.spark.daria.sql
 
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.Row
-import org.apache.spark.sql.types.{StructField, StructType}
+import org.apache.spark.sql.types.{DataType, StructField, StructType}
 
 object SparkSessionExt {
 
@@ -13,6 +13,35 @@ object SparkSessionExt {
         spark.sparkContext.parallelize(rowData),
         StructType(schema)
       )
+    }
+
+    // weird method signature per this Stackoverflow thread: http://stackoverflow.com/a/4982668/1125159
+    def createDF(
+      rowData: List[Row],
+      fields: List[(String, DataType, Boolean)]
+    )(implicit i1: DummyImplicit): DataFrame = {
+      val structFields = fields.map(field => {
+        StructField(field._1, field._2, field._3)
+      })
+      spark.createDataFrame(
+        spark.sparkContext.parallelize(rowData),
+        StructType(structFields)
+      )
+    }
+
+    implicit def value2tuple[T](x: T): Tuple1[T] = Tuple1(x)
+
+    def createDF(
+      values: List[Product],
+      fields: List[(String, DataType, Boolean)]
+    )(implicit i1: DummyImplicit, i2: DummyImplicit): DataFrame = {
+      val rows = values.map(value => {
+        Row(value.productIterator.toList: _*)
+      })
+      val structFields = fields.map(field => {
+        StructField(field._1, field._2, field._3)
+      })
+      spark.createDF(rows, structFields)
     }
 
   }
