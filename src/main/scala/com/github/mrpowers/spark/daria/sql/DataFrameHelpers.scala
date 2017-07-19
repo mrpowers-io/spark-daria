04307f8708de010e863cf5f257bf882699e6171f
MrPowers <matthewkevinpowers@gmail.com>
2017-07-19 14:05:35 -0400

    Started adding some map lookup code and failed

--- src/main/scala/com/github/mrpowers/spark/daria/sql/DataFrameHelpers.scala
+++ src/main/scala/com/github/mrpowers/spark/daria/sql/DataFrameHelpers.scala
@@ -9,4 +9,16 @@ object DataFrameHelpers {
     df.collect.map(r => Map(df.columns.zip(r.toSeq): _*))
   }
 
+  def withValueLookup(
+    df: DataFrame,
+    maps: Array[Map[String, Any]],
+    mapValue: String,
+    keyMappings: Map[String, String]
+  ): DataFrame = {
+    df.withColumn(
+      mapValue,
+      lit("hi")
+    )
+  }
+
 }
