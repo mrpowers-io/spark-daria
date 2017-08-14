3f76b729d991bf6ee252eefc1c3a4695d5ea8e39
MrPowers <matthewkevinpowers@gmail.com>
2017-08-14 23:34:11 +0800

    Add a sortColumns transformation to alphabetize the columns in a DataFrame

--- src/main/scala/com/github/mrpowers/spark/daria/sql/transformations/package.scala
+++ src/main/scala/com/github/mrpowers/spark/daria/sql/transformations/package.scala
@@ -1,8 +1,23 @@
 package com.github.mrpowers.spark.daria.sql
 
 import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.functions.col
+
+case class InvalidColumnSortOrderException(smth: String) extends Exception(smth)
 
 package object transformations {
+
+  def sortColumns(order: String = "asc")(df: DataFrame): DataFrame = {
+    val cols = if (order == "asc") {
+      df.columns.sorted
+    } else if (order == "desc") {
+      df.columns.sorted.reverse
+    } else {
+      val message = s"The sort order must be 'asc' or 'desc'.  Your sort order was '$order'."
+      throw new InvalidColumnSortOrderException(message)
+    }
+    df.select(cols.map(col): _*)
+  }
 
   def snakeCaseColumns(df: DataFrame): DataFrame = {
     df.columns.foldLeft(df) { (memoDF, colName) =>
--- src/test/scala/com/github/mrpowers/spark/daria/sql/TransformationsSpec.scala
+++ src/test/scala/com/github/mrpowers/spark/daria/sql/TransformationsSpec.scala
@@ -2,13 +2,91 @@ package com.github.mrpowers.spark.daria.sql
 
 import com.github.mrpowers.spark.fast.tests.DataFrameComparer
 import org.scalatest.FunSpec
-import org.apache.spark.sql.types.StringType
+import org.apache.spark.sql.types.{StringType, IntegerType}
 import com.github.mrpowers.spark.daria.sql.SparkSessionExt._
 
 class TransformationsSpec
     extends FunSpec
     with DataFrameComparer
     with SparkSessionTestWrapper {
+
+  describe("#sortColumns") {
+
+    it("sorts the columns in a DataFrame in ascending order") {
+
+      val sourceDF = spark.createDF(
+        List(
+          ("pablo", 3, "polo")
+        ), List(
+          ("name", StringType, true),
+          ("age", IntegerType, true),
+          ("sport", StringType, true)
+        )
+      )
+
+      val actualDF = sourceDF.transform(transformations.sortColumns())
+
+      val expectedDF = spark.createDF(
+        List(
+          (3, "pablo", "polo")
+        ), List(
+          ("age", IntegerType, true),
+          ("name", StringType, true),
+          ("sport", StringType, true)
+        )
+      )
+
+      assertSmallDataFrameEquality(actualDF, expectedDF)
+
+    }
+
+    it("sorts the columns in a DataFrame in descending order") {
+
+      val sourceDF = spark.createDF(
+        List(
+          ("pablo", 3, "polo")
+        ), List(
+          ("name", StringType, true),
+          ("age", IntegerType, true),
+          ("sport", StringType, true)
+        )
+      )
+
+      val actualDF = sourceDF.transform(transformations.sortColumns("desc"))
+
+      val expectedDF = spark.createDF(
+        List(
+          ("polo", "pablo", 3)
+        ), List(
+          ("sport", StringType, true),
+          ("name", StringType, true),
+          ("age", IntegerType, true)
+        )
+      )
+
+      assertSmallDataFrameEquality(actualDF, expectedDF)
+
+    }
+
+    it("throws an error if the sort order is invalid") {
+
+      val sourceDF = spark.createDF(
+        List(
+          ("pablo", 3, "polo")
+        ), List(
+          ("name", StringType, true),
+          ("age", IntegerType, true),
+          ("sport", StringType, true)
+        )
+      )
+
+      intercept[InvalidColumnSortOrderException] {
+        sourceDF.transform(transformations.sortColumns("cats"))
+      }
+
+    }
+
+  }
 
   describe("#snakeCaseColumns") {
 
