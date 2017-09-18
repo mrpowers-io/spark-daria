0cd6ff0cb672894d0756d3b06019c4ebc1c80709
MrPowers <matthewkevinpowers@gmail.com>
2017-09-17 21:07:19 -0500

    Simplify the implementation of the twoColumnsToMap method

--- src/main/scala/com/github/mrpowers/spark/daria/sql/DataFrameHelpers.scala
+++ src/main/scala/com/github/mrpowers/spark/daria/sql/DataFrameHelpers.scala
@@ -13,14 +13,13 @@ object DataFrameHelpers extends DataFrameValidator {
     df: DataFrame,
     keyColName: String,
     valueColName: String
-  ): collection.mutable.Map[keyType, valueType] = {
+  ): Map[keyType, valueType] = {
     validatePresenceOfColumns(df, Seq(keyColName, valueColName))
     df
       .select(keyColName, valueColName)
       .collect()
-      .foldLeft(collection.mutable.Map.empty[keyType, valueType]) { (memo, arr) =>
-        memo += (arr(0).asInstanceOf[keyType] -> arr(1).asInstanceOf[valueType])
-      }
+      .map(r => (r(0).asInstanceOf[keyType], r(1).asInstanceOf[valueType]))
+      .toMap
   }
 
   def columnToArray[T: ClassTag](
