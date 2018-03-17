52aca163c1598022eb99131f6db82a43fdb071f1
MrPowers <matthewkevinpowers@gmail.com>
2018-03-17 14:35:08 -0500

    Update the deploy script to include the Spark version in the tag and the release to allow for multi project builds

--- scripts/github_release.sh
+++ scripts/github_release.sh
@@ -59,10 +59,12 @@ if [ "$SPARK_VERSION" = "" ]
     exit 1
 fi
 
+SPARK_PROJECT_VERSION=${SPARK_VERSION}_${PROJECT_VERSION}
+
 LATEST_GIT_TAG="$(git describe --abbrev=0 --tags)"
-if [ $LATEST_GIT_TAG = v$PROJECT_VERSION ]
+if [ $LATEST_GIT_TAG = v$SPARK_PROJECT_VERSION ]
   then
-    echo "The git tag $LATEST_GIT_TAG already exists.  The project version (currently $PROJECT_VERSION) needs to be bumped in the build.sbt file."
+    echo "The git tag $LATEST_GIT_TAG already exists.  The project version (currently $SPARK_PROJECT_VERSION) needs to be bumped in the build.sbt file."
     exit 1
 fi
 
@@ -71,21 +73,22 @@ echo Project version $PROJECT_VERSION
 echo Scala version $SCALA_VERSION
 echo Scala binary version $SCALA_BINARY_VERSION
 echo Spark version $SPARK_VERSION
+echo SPARK_PROJECT_VERSION $SPARK_PROJECT_VERSION
 
 echo "Create a git commit to bump the version"
-git commit -am "Version bump to $PROJECT_VERSION"
+git commit -am "Version bump to $SPARK_PROJECT_VERSION"
 
 echo "Push the version bump commit to master"
 git push origin master
 
 echo "Create a tag"
-git tag v$PROJECT_VERSION
-git push origin v$PROJECT_VERSION
+git tag v$SPARK_PROJECT_VERSION
+git push origin v$SPARK_PROJECT_VERSION
 
 echo "Create a JAR file"
 sbt $JAR_CREATION_METHOD
 
 echo "Create a GitHub release"
 JAR_PATH=target/scala-${SCALA_BINARY_VERSION}/${PROJECT_NAME}_${SCALA_BINARY_VERSION}-${SPARK_VERSION}_${PROJECT_VERSION}.jar
-hub release create -a $JAR_PATH -m "Release v${PROJECT_VERSION}" v${PROJECT_VERSION}
+hub release create -a $JAR_PATH -m "Release v${SPARK_PROJECT_VERSION}" v${SPARK_PROJECT_VERSION}
 
