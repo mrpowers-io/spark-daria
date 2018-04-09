637d7cc6118d746a382bbe0eea6b42982c7d4b72
MrPowers <matthewkevinpowers@gmail.com>
2018-04-08 22:04:32 -0400

    Add a script to make multiple releases that can get picked up by JitPack

---
+++ scripts/multiple_spark_releases.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+SPARK_DARIA_GITHUB_RELEASE=$1
+if [ "$SPARK_DARIA_GITHUB_RELEASE" = "" ]
+  then
+    echo "spark-daria github_release script path must be set"
+    exit 1
+fi
+
+for sparkVersion in 2.2.0 2.2.1 2.3.0; do
+  echo $sparkVersion
+  sed -i '' "s/^val sparkVersion.*/val sparkVersion = \"$sparkVersion\"/" build.sbt
+  $SPARK_DARIA_GITHUB_RELEASE assembly
+done
