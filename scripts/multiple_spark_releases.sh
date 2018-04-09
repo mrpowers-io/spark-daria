606e81ebaca9e9db042054f1e4d313a4032225de
MrPowers <matthewkevinpowers@gmail.com>
2018-04-08 23:58:03 -0400

    Version bump to 2.3.0_0.19.0

--- scripts/multiple_spark_releases.sh
+++ scripts/multiple_spark_releases.sh
@@ -8,8 +8,8 @@ if [ "$SPARK_DARIA_GITHUB_RELEASE" = "" ]
 fi
 
 #for sparkVersion in 2.2.0 2.2.1 2.3.0; do
-for sparkVersion in 2.2.1; do
+for sparkVersion in 2.3.0; do
   echo $sparkVersion
   sed -i '' "s/^val sparkVersion.*/val sparkVersion = \"$sparkVersion\"/" build.sbt
-  $SPARK_DARIA_GITHUB_RELEASE assembly
+  $SPARK_DARIA_GITHUB_RELEASE package
 done
