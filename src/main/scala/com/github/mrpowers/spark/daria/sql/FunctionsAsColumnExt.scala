4bdf8a947c57408e94012236b0b8d6822243be10
MrPowers <matthewkevinpowers@gmail.com>
2018-11-27 08:55:10 -0500

    Make additional functions accessible as column extensions

--- src/main/scala/com/github/mrpowers/spark/daria/sql/FunctionsAsColumnExt.scala
+++ src/main/scala/com/github/mrpowers/spark/daria/sql/FunctionsAsColumnExt.scala
@@ -6,8 +6,24 @@ object FunctionsAsColumnExt {
 
   implicit class ColumnMethods(col: Column) {
 
+    def initcap(): Column = {
+      org.apache.spark.sql.functions.initcap(col)
+    }
+
+    def length(): Column = {
+      org.apache.spark.sql.functions.length(col)
+    }
+
     def lower(): Column = {
       org.apache.spark.sql.functions.lower(col)
+    }
+
+    def regexp_replace(pattern: String, replacement: String): Column = {
+      org.apache.spark.sql.functions.regexp_replace(
+        col,
+        pattern,
+        replacement
+      )
     }
 
   }
--- src/test/scala/com/github/mrpowers/spark/daria/sql/FunctionsAsColumnExtTest.scala
+++ src/test/scala/com/github/mrpowers/spark/daria/sql/FunctionsAsColumnExtTest.scala
@@ -13,34 +13,84 @@ object FunctionsAsColumnExtTest
 
   val tests = Tests {
 
-    'lower - {
+    'initcap - {
 
-      "lower cases a column" - {
-
-        val df = spark
-          .createDF(
-            List(
-              ("ThIS is COOL", "this is cool"),
-              ("HAPPy", "happy"),
-              (null, null)
-            ),
-            List(
-              ("some_string", StringType, true),
-              ("expected", StringType, true)
-            )
+      val df = spark
+        .createDF(
+          List(
+            ("ThIS is COOL", "This Is Cool"),
+            ("HAPPy", "Happy"),
+            (null, null)
+          ),
+          List(
+            ("some_string", StringType, true),
+            ("expected", StringType, true)
           )
-          .withColumn(
-            "some_string_lower",
-            col("some_string").lower()
-          )
-
-        assertColumnEquality(
-          df,
-          "expected",
-          "some_string_lower"
+        )
+        .withColumn(
+          "res",
+          col("some_string").initcap()
         )
 
-      }
+      assertColumnEquality(
+        df,
+        "expected",
+        "res"
+      )
+
+    }
+
+    'length - {
+
+      val df = spark
+        .createDF(
+          List(
+            ("this", 4),
+            ("hi", 2),
+            (null, null)
+          ),
+          List(
+            ("some_string", StringType, true),
+            ("expected", IntegerType, true)
+          )
+        )
+        .withColumn(
+          "res",
+          col("some_string").length()
+        )
+
+      assertColumnEquality(
+        df,
+        "expected",
+        "res"
+      )
+
+    }
+
+    'lower - {
+
+      val df = spark
+        .createDF(
+          List(
+            ("ThIS is COOL", "this is cool"),
+            ("HAPPy", "happy"),
+            (null, null)
+          ),
+          List(
+            ("some_string", StringType, true),
+            ("expected", StringType, true)
+          )
+        )
+        .withColumn(
+          "some_string_lower",
+          col("some_string").lower()
+        )
+
+      assertColumnEquality(
+        df,
+        "expected",
+        "some_string_lower"
+      )
 
     }
 
