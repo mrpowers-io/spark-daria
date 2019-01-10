3ae26f67585d1d97787c9fafc027c9e32abfb03f
kirubakarrs <kirubakar@prognos.ai>
2019-01-10 14:47:40 -0500

    Update sqlCharsToEscape in StringHelpers object

--- src/main/scala/com/github/mrpowers/spark/daria/utils/StringHelpers.scala
+++ src/main/scala/com/github/mrpowers/spark/daria/utils/StringHelpers.scala
@@ -6,7 +6,7 @@ object StringHelpers {
    * All the characters that need to be escaped for SQL regexp
    *
    */
-  val sqlCharsToEscape = "()/-.'|".map { c: Char =>
+  val sqlCharsToEscape = "()/-.'|+".map { c: Char =>
     "\\" + c
   }.toList
 
@@ -16,7 +16,9 @@ object StringHelpers {
    *
    */
   def escapeForSqlRegexp(str: String, charsToEscape: List[String] = sqlCharsToEscape): Option[String] = {
+
     val s = Option(str).getOrElse(return None)
+
     Some(charsToEscape.foldLeft(str) {
       case (res, pattern) =>
         res.replaceAll(
