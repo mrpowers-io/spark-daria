79ade6f838aea2c6739aa72489294b527a243a85
MrPowers <matthewkevinpowers@gmail.com>
2019-10-07 22:09:27 -0400

    Add a helper to calculate the number of 1GB files per each partition in a partitioned Delta lake

--- src/main/scala/com/github/mrpowers/spark/daria/delta/DeltaLogHelpers.scala
+++ src/main/scala/com/github/mrpowers/spark/daria/delta/DeltaLogHelpers.scala
@@ -16,4 +16,25 @@ object DeltaLogHelpers {
 
   }
 
+  def withBytesToGigabytes(bytesColName: String, outputColName: String = "gigabytes")(df: DataFrame): DataFrame = {
+    df.withColumn(outputColName, col(bytesColName) / lit(1073741824L))
+  }
+
+  def partitionedLake1GbChunks(deltaLogDF: DataFrame, partitionKey: String, minNumPartitions: Int = 1): DataFrame = {
+
+    deltaLogDF
+      .withColumn("partitionValue", col("partitionValues")(partitionKey))
+      .groupBy("partitionValue")
+      .sum()
+      .transform(withBytesToGigabytes("sum(size)"))
+      .withColumn(
+        "num_1GB_partitions",
+        when(
+          col("gigabytes") < minNumPartitions,
+          minNumPartitions
+        ).otherwise(col("gigabytes").cast("int"))
+      )
+
+  }
+
 }
--- src/test/scala/com/github/mrpowers/spark/daria/delta/DeltaLogHelpersTest.scala
+++ src/test/scala/com/github/mrpowers/spark/daria/delta/DeltaLogHelpersTest.scala
@@ -1,13 +1,13 @@
 package com.github.mrpowers.spark.daria.delta
 
 import utest._
-
 import com.github.mrpowers.spark.daria.sql.SparkSessionTestWrapper
-import com.github.mrpowers.spark.fast.tests.ColumnComparer
+import com.github.mrpowers.spark.fast.tests.DataFrameComparer
 import com.github.mrpowers.spark.daria.sql.SparkSessionExt._
 import org.apache.spark.sql.types._
+import org.apache.spark.sql.functions._
 
-object DeltaLogHelpersTest extends TestSuite with ColumnComparer with SparkSessionTestWrapper {
+object DeltaLogHelpersTest extends TestSuite with DataFrameComparer with SparkSessionTestWrapper {
 
   val tests = Tests {
 
@@ -60,6 +60,45 @@ object DeltaLogHelpersTest extends TestSuite with ColumnComparer with SparkSessi
         )
 
         assert(DeltaLogHelpers.num1GbPartitions(df) == 2444)
+
+      }
+
+    }
+
+    'partitionedLake1GbChunks - {
+
+      "it computes the 1GB chunks per partition" - {
+
+        val df = spark.createDF(
+          List(
+            ("some_file.snappy.parquet", Map("country" -> "China"), 234234234940L),
+            ("some_file.snappy.parquet", Map("country" -> "China"), 1000L),
+            ("some_file.snappy.parquet", Map("country" -> "India"), 50L),
+            ("some_file.snappy.parquet", Map("country" -> "Japan"), 50L)
+          ),
+          List(
+            ("path", StringType, true),
+            ("partitionValues", MapType(StringType, StringType, true), true),
+            ("size", LongType, true)
+          )
+        )
+
+        val expectedDF = spark.createDF(
+          List(
+            ("China", 218),
+            ("India", 1),
+            ("Japan", 1)
+          ),
+          List(
+            ("partitionValue", StringType, true),
+            ("num_1GB_partitions", IntegerType, true)
+          )
+        )
+
+        assertSmallDataFrameEquality(
+          DeltaLogHelpers.partitionedLake1GbChunks(df, "country").select("partitionValue", "num_1GB_partitions"),
+          expectedDF
+        )
 
       }
 
