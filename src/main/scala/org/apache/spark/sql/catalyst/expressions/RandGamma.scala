901819eb4603e5e4ec0eeb41b0128b7b19ad956f
Tuan Pham <Tuan.Pham@wisetechglobal.com>
2024-09-22 15:20:24 +1000

    add test for gamma and laplace distribution column function

--- src/main/scala/org/apache/spark/sql/catalyst/expressions/RandGamma.scala
+++ src/main/scala/org/apache/spark/sql/catalyst/expressions/RandGamma.scala
@@ -26,10 +26,11 @@ case class RandGamma(child: Expression, shape: Expression, scale: Expression, hi
     case LongType => shape.eval().asInstanceOf[Long]
     case FloatType | DoubleType => shape.eval().asInstanceOf[Double]
   }
-  @transient protected lazy val scaleVal: Double = shape.dataType match {
-    case IntegerType => shape.eval().asInstanceOf[Int]
-    case LongType => shape.eval().asInstanceOf[Long]
-    case FloatType | DoubleType => shape.eval().asInstanceOf[Double]
+
+  @transient protected lazy val scaleVal: Double = scale.dataType match {
+    case IntegerType => scale.eval().asInstanceOf[Int]
+    case LongType => scale.eval().asInstanceOf[Long]
+    case FloatType | DoubleType => scale.eval().asInstanceOf[Double]
   }
 
   override protected def initializeInternal(partitionIndex: Int): Unit = {
@@ -39,7 +40,7 @@ case class RandGamma(child: Expression, shape: Expression, scale: Expression, hi
 
   def this() = this(UnresolvedSeed, Literal(1.0, DoubleType), Literal(1.0, DoubleType), true)
 
-  def this(child: Expression) = this(child, Literal(1.0, DoubleType), Literal(1.0, DoubleType), false)
+  def this(child: Expression, shape: Expression, scale: Expression) = this(child, shape, scale, false)
 
   override def withNewSeed(seed: Long): RandGamma = RandGamma(Literal(seed, LongType), shape, scale, hideSeed)
 
@@ -59,9 +60,10 @@ case class RandGamma(child: Expression, shape: Expression, scale: Expression, hi
   override def freshCopy(): RandGamma = RandGamma(child, shape, scale, hideSeed)
 
   override def flatArguments: Iterator[Any] = Iterator(child)
-//  override def sql: String = {
-//    s"rand_gamma(${if (hideSeed) "" else child.sql})"
-//  }
+
+  override def prettyName: String = "rand_gamma"
+
+  override def sql: String = s"rand_gamma(${if (hideSeed) "" else s"${child.sql}, ${shape.sql}, ${scale.sql}"})"
 
   override def inputTypes: Seq[AbstractDataType] = Seq(LongType, DoubleType, DoubleType)
 
@@ -73,10 +75,10 @@ case class RandGamma(child: Expression, shape: Expression, scale: Expression, hi
 
   override def third: Expression = scale
 
-  override protected def withNewChildrenInternal(newFirst: Expression, newSecond: Expression, newThird: Expression): Expression = copy(child = newFirst, shape = newSecond, scale = newThird)
+  override protected def withNewChildrenInternal(newFirst: Expression, newSecond: Expression, newThird: Expression): Expression =
+    copy(child = newFirst, shape = newSecond, scale = newThird)
 }
 
 object RandGamma {
   def apply(seed: Long, shape: Double, scale: Double): RandGamma = RandGamma(Literal(seed, LongType), Literal(shape, DoubleType), Literal(scale, DoubleType))
 }
-
--- src/main/scala/org/apache/spark/sql/daria/functions.scala
+++ src/main/scala/org/apache/spark/sql/daria/functions.scala
@@ -1,12 +1,16 @@
-﻿package org.apache.spark.sql
+package org.apache.spark.sql.daria
 
+import org.apache.spark.sql.Column
 import org.apache.spark.sql.catalyst.expressions.{Expression, RandGamma}
 import org.apache.spark.sql.functions.{lit, log, rand, when}
+import org.apache.spark.util.Utils
 
-object customFunctions {
+object functions {
   private def withExpr(expr: Expression): Column = Column(expr)
 
   def randGamma(seed: Long, shape: Double, scale: Double): Column = withExpr(RandGamma(seed, shape, scale)).alias("gamma_random")
+  def randGamma(shape: Double, scale: Double): Column = randGamma(Utils.random.nextLong, shape, scale)
+  def randGamma(): Column = randGamma(1.0, 1.0)
 
   def randLaplace(seed: Long, mu: Double, beta: Double): Column = {
     val mu_ = lit(mu)
@@ -16,4 +20,7 @@ object customFunctions {
       .otherwise(mu_ - beta_ * log(lit(2) * (lit(1) - u)))
       .alias("laplace_random")
   }
+
+  def randLaplace(mu: Double, beta: Double): Column = randLaplace(Utils.random.nextLong, mu, beta)
+  def randLaplace(): Column = randLaplace(0.0, 1.0)
 }
