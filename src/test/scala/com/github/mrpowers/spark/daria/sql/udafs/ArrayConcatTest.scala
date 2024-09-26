5ed04cbfa308a4478de293c069f3b5a98a6c11c5
zeotuan <48720253+zeotuan@users.noreply.github.com>
2024-09-26 20:02:18 +1000

    add arrayConcatAggregator (#159)

--- src/test/scala/com/github/mrpowers/spark/daria/sql/udafs/ArrayConcatTest.scala
+++ src/test/scala/com/github/mrpowers/spark/daria/sql/udafs/ArrayConcatTest.scala
@@ -54,6 +54,47 @@ object ArrayConcatTest extends TestSuite with DataFrameComparer with SparkSessio
 
       }
 
+      "concatenates rows of arrays using aggregator" - {
+
+        val arrayConcat = udaf(new ArrayConcatAggregator[String]())
+
+        val actualDF = spark
+          .createDF(
+            List(
+              Array(
+                "snake",
+                "rat"
+              ),
+              null,
+              Array(
+                "cat",
+                "crazy"
+              )
+            ),
+            List(("array", ArrayType(StringType), true))
+          )
+          .agg(arrayConcat(col("array")).as("array"))
+
+        val expectedDF = spark
+          .createDF(
+            List(
+              Array(
+                "snake",
+                "rat",
+                "cat",
+                "crazy"
+              )
+            ),
+            List(("array", ArrayType(StringType), true))
+          )
+
+        assertSmallDataFrameEquality(
+          actualDF,
+          expectedDF
+        )
+
+      }
+
     }
 
   }
