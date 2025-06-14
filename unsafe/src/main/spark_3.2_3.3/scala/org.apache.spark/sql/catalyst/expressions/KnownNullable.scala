8ad497461d7b92b22805efc66f378bce8139c88a
Tuan Pham <zeotuan@gmail.com>
2025-06-14 16:26:28 +1000

    KnownNullable

---
+++ unsafe/src/main/spark_3.2_3.3/scala/org.apache.spark/sql/catalyst/expressions/KnownNullable.scala
@@ -0,0 +1,18 @@
+package org.apache.spark.sql.catalyst.expressions
+
+import org.apache.spark.sql.catalyst.InternalRow
+import org.apache.spark.sql.catalyst.expressions.codegen.{CodegenContext, ExprCode, FalseLiteral}
+import org.apache.spark.sql.types.DataType
+
+case class KnownNullable(child: Expression) extends UnaryExpression {
+  override def dataType: DataType = child.dataType
+
+  override def nullable: Boolean = true
+
+  override protected def doGenCode(ctx: CodegenContext, ev: ExprCode): ExprCode = {
+    child.genCode(ctx)
+  }
+
+  override protected def withNewChildInternal(newChild: Expression): KnownNullable =
+    copy(child = newChild)
+}
