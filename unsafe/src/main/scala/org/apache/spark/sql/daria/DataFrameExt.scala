d1b7de628d879b3db751502b8ec4484c9a40c4f1
Tuan Pham <zeotuan@gmail.com>
2025-06-15 21:09:32 +1000

    Remove reliant on DataFrame.to

--- unsafe/src/main/scala/org/apache/spark/sql/daria/DataFrameExt.scala
+++ unsafe/src/main/scala/org/apache/spark/sql/daria/DataFrameExt.scala
@@ -1,4 +1,5 @@
 package org.apache.spark.sql.daria
+
 import org.apache.spark.sql.daria.functions.{assertNotNull, knownNotNull, knownNullable}
 import org.apache.spark.sql.functions.{col, struct, transform}
 import org.apache.spark.sql.types.{ArrayType, StructField, StructType}
@@ -8,73 +9,77 @@ object DataFrameExt {
 
   /**
    * Aligns the nullability of the columns in the original schema to match the target schema.
-   * Will ignore columns that are not present in the original schema.
-   * Will not cast from original data type to target data type.
+   * Will still include columns that are not present in the original schema, but will not assert nullability on them.
+   * Will use target schema data types if they differ from the original schema.
    */
-  private def alignSchemaNullability(originalSchema: StructType, targetSchema: StructType, alignNotNullToNullable: Boolean): Seq[Column] = {
-    def childFieldToCol(orgChildField: StructField, targetChildField: StructField, parentCol: Column, firstLevel: Boolean = false): Column = {
-      val fieldCol = (orgChildField.dataType, targetChildField.dataType) match {
-        case (orgStruct: StructType, tarStruct: StructType) =>
-          val fields = orgStruct.map { orgField =>
-            tarStruct.find(_.name == orgField.name) match {
-              case Some(tarField) =>
+  private def alignSchemaNullability(
+      originalSchema: StructType,
+      targetSchema: StructType,
+      alignNotNullToNullable: Boolean
+  ): Seq[Column] = {
+    def childFieldToCol(
+        orgChildField: Option[StructField],
+        targetChildField: StructField,
+        parentCol: Column,
+        firstLevel: Boolean = false
+    ): Column = {
+      orgChildField match {
+        case Some(oField) =>
+          val fieldCol = (oField.dataType, targetChildField.dataType) match {
+            case (orgStruct: StructType, tarStruct: StructType) =>
+              val fields = tarStruct.map { tarField =>
                 childFieldToCol(
-                  orgField,
+                  orgStruct.find(_.name == tarField.name),
                   tarField,
-                  orgField.dataType match {
-                    case StructType(_) | ArrayType(_: StructType, _) => parentCol(orgField.name)
+                  tarField.dataType match {
+                    case StructType(_) | ArrayType(_: StructType, _) => parentCol(tarField.name)
                     case _                                           => parentCol
                   }
-                ).as(orgField.name)
-              case None => parentCol(orgField.name).as(orgField.name)
-            }
+                ).as(tarField.name)
+              }
+
+              struct(fields: _*)
+            case (ArrayType(oInnerType, _), ArrayType(tInnerType, _)) =>
+              transform(
+                parentCol,
+                childCol => childFieldToCol(Some(oField.copy(dataType = oInnerType)), targetChildField.copy(dataType = tInnerType), childCol)
+              )
+            case _ if firstLevel  => parentCol
+            case _ if !firstLevel => parentCol(targetChildField.name)
           }
 
-          struct(fields: _*)
-        case (ArrayType(oInnerType, _), ArrayType(tInnerType, _)) =>
-          transform(
-            parentCol,
-            childCol => childFieldToCol(orgChildField.copy(dataType = oInnerType), targetChildField.copy(dataType = tInnerType), childCol)
-          )
-        case _ if firstLevel  => parentCol
-        case _ if !firstLevel => parentCol(orgChildField.name)
-      }
+          val alignedField = (targetChildField.nullable, oField.nullable, alignNotNullToNullable) match {
+            case (false, true, _)     => assertNotNull(fieldCol)
+            case (true, false, true)  => knownNullable(fieldCol)
+            case (true, false, false) => fieldCol
+            case (false, false, _)    => knownNotNull(fieldCol)
+            case (true, true, _)      => knownNullable(fieldCol)
+          }
 
-      val alignedField = (targetChildField.nullable, orgChildField.nullable, alignNotNullToNullable) match {
-        case (false, true, _)     => assertNotNull(fieldCol)
-        case (true, false, true)  => knownNullable(fieldCol)
-        case (true, false, false) => fieldCol
-        case (false, false, _)    => knownNotNull(fieldCol)
-        case (true, true, _)      => knownNullable(fieldCol)
+          alignedField.as(targetChildField.name)
+        case None if firstLevel  => parentCol
+        case None if !firstLevel => parentCol(targetChildField.name)
       }
-
-      alignedField.as(orgChildField.name)
     }
 
-    originalSchema.map { oField =>
-      val f = targetSchema.find(_.name == oField.name) match {
-        case Some(tField) => childFieldToCol(oField, tField, col(oField.name), firstLevel = true)
-        case None         => col(oField.name)
-      }
-      f.as(oField.name)
-    }
+    targetSchema.map(tField => childFieldToCol(originalSchema.find(_.name == tField.name), tField, col(tField.name), firstLevel = true))
   }
 
   implicit class DataFrameMethods(df: DataFrame) {
 
     /**
      * Converts the DataFrame to the specified target schema, aligning nullability if required.
-     * This is essentially the same as `df.to(targetSchema)` but allows for nullability alignment by asserting nullability
+     * This is similar to `df.to(targetSchema)` but allows for nullability alignment by asserting nullability
      * for each column where the original nullability does not match the target nullability.
      *
-     * @param targetSchema The target schema to convert to.
+     * @param targetSchema           The target schema to convert to.
      * @param alignNotNullToNullable by default if original column is not nullable and target column is nullable, it will not make it nullable.
-     *                        If set to true, it will align target Dataframe column to nullable.
+     *                               If set to true, it will align target Dataframe column to nullable.
      * @return A new DataFrame with the specified schema.
      */
     def toSchemaWithNullabilityAligned(targetSchema: StructType, alignNotNullToNullable: Boolean = false): DataFrame = {
       val columns = alignSchemaNullability(df.schema, targetSchema, alignNotNullToNullable)
-      df.select(columns: _*).to(targetSchema)
+      df.select(columns: _*)
     }
   }
 }
